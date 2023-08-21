0a98b526533242aac8c58a05636b04e96dadefa3
TASKIN AHMAD AL FARUQE <119452412+taskinahmadalfaruqe@users.noreply.github.com>
2023-08-21 16:52:15 +0600

    money master add

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    container:{
+      center: true,
+      background: '#111',
+      padding:{
+        DEFAULT: '10px',
+        md: '1rem',
+        lg: '2rem',
+        xl: '2rem',
+        '2xl': '3rem',
+      }
+    },
+    extend: {},
+  },
+  plugins: [],
+
+}
+
